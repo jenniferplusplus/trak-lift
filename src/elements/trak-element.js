8d57d9cc63375cc7198dfddacae93dc7a146ce11
Jenniferplusplus <contac@jenniferplusplus.com>
2025-06-10 01:35:19 -0500

    initial commit dev setup and initial models

---
+++ src/elements/trak-element.js
@@ -0,0 +1,43 @@
+import {css, LitElement} from 'lit'
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+export class TrakElement extends LitElement {
+  constructor() {
+    super()
+  }
+
+  static get styles() {
+    return getGlobalStyleSheets();
+  }
+
+  init(){
+    useGlobalStyles(this.shadowRoot);
+  }
+}
+
+let globalSheets = null;
+
+export function getGlobalStyleSheets() {
+  if (globalSheets === null) {
+    globalSheets = Array.from(document.styleSheets)
+        .map(x => {
+          const sheet = new CSSStyleSheet();
+          const css = Array.from(x.cssRules).map(rule => rule.cssText).join(' ');
+          sheet.replaceSync(css);
+          return sheet;
+        });
+  }
+
+  return globalSheets;
+}
+
+export function useGlobalStyles(shadowRoot) {
+  shadowRoot.adoptedStyleSheets.push(
+      ...getGlobalStyleSheets()
+  );
+}
---
+++ src/models.js
@@ -0,0 +1,77 @@
+'use strict';
+
+export class Exercise {
+    kind = 'exercise';
+    /** @var {string} name */
+    name;
+    /** @var {Date} [start] */
+    start;
+    /** @var {Date} [stop] */
+    stop;
+    /** @var {URL} [guide] */
+    guide;
+}
+
+export class ExerciseWeight extends Exercise {
+    kind = 'weight';
+    /** @var {Number} weight */
+    weight;
+    /** @var {Number} reps */
+    reps;
+    /** @var {Number} sets=1 */
+    sets;
+}
+
+export class ExerciseDistance extends Exercise {
+    kind = 'distance';
+    /** @var {Number} distance */
+    distance;
+    /** @var {string} unit */
+    unit;
+}
+
+export class ExerciseEffort extends Exercise {
+    kind = 'effort';
+    /** @var {Number} effort */
+    effort;
+}
+
+export class Routine {
+    /** @var {string} name */
+    name;
+    /** @var {[Exercise]} exercises */
+    exercises = [];
+}
+
+export class Plan {
+    /** @var {string} kind */
+    kind;
+    /** @var {string} name */
+    name;
+    /** @var {[Routine]} routines */
+    routines = [];
+}
+
+export class PlanSchedule extends Plan {
+    kind = 'schedule';
+    /** @var {CronString} schedule */
+    schedule;
+}
+
+export class PlanFlow extends Plan {
+    kind = 'flow';
+    /** @var {FlowString} flow */
+    flow;
+    // TBD, but I'm imagining something like E = exercise, R = rest, _ = range
+    // Ex: E_ R E__
+    // day 1 or 2: exercise
+    // day 3: rest
+    // day 4, 5 or 6: exercise
+}
+
+export class Session {
+    start;
+    stop;
+    exercises;
+    routine;
+}
