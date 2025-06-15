230ce58a55a1810f2100135b390d82860dbc461c
Jenniferplusplus <contac@jenniferplusplus.com>
2025-06-14 23:30:57 -0500

    todo list

--- src/routes/home.js
+++ src/routes/home.js
@@ -8,5 +8,12 @@ export default function home(match) {
     Render(`
         <h1>Welcome home!</h1>
         <a href="/about" data-navigo>About</a>
+        <h2>todo</h2>
+        <ol>
+            <li>manage your routines</li>
+            <li>start a session</li>
+            <li>session history</li>
+            <li>manage your exercises</li>
+        </ol>
     `)
 }
