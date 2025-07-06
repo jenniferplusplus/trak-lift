cf1e450d89f0fc5879d40eac9cac79a64d03d16c
Jenniferplusplus <contac@jenniferplusplus.com>
2025-07-06 00:39:54 -0500

    404 page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
-export const base ='/trak-lift/';
+export const base = '/trak-lift/';
 
 export default {
-    base: base
+    base: base,
+    build: {
+        rollupOptions: {
+            input: {
+                main: `${__dirname}/index.html`,
+                notFound: `${__dirname}/404.html`,
+            }
+        }
+    }
 }
